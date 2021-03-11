f9f318434dab5c28e6c51dbd1673beecc948785f
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2021-03-11 17:29:35 +0000

    add accent

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import re
 from pathlib import Path
 
 import numpy
@@ -19,19 +20,44 @@ from inference_hifigan import inference_hifigan
 
 
 def run(text: str, speaker_id: int):
-    phoneme_list = openjtalk_label_getter(
+    # phoneme
+    label_list = openjtalk_label_getter(
         text=text,
         openjtalk_command="open_jtalk",
         dict_path=Path("/var/lib/mecab/dic/open-jtalk/naist-jdic"),
         htsvoice_path=Path(
             "/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice"
         ),
-        output_wave_path=None,
-        output_log_path=None,
-        output_type=OutputType.phoneme,
+        output_wave_path="hiho_openjtalk_wave.wav",
+        output_log_path="hiho_openjtalk_log.txt",
+        output_type=OutputType.full_context_label,
     )
-    print(phoneme_list)
-    json.dump([p.label for p in phoneme_list], open("hiho_phoneme_list.json", mode="w"))
+    json.dump([p.label for p in label_list], open("hiho_phoneme_list.json", mode="w"))
+
+    is_type1 = False
+    phoneme_list = label_list
+    start_accent_list = []
+    end_accent_list = []
+    for i, label in enumerate(label_list):
+        phoneme, a, b = re.match(
+            r".+?\^.+?\-(.+?)\+.+?\=.+?/A:(.+?)\+(.+?)\+.+?/B:.+", label.label
+        ).groups()
+
+        is_end_accent = a == "0"
+
+        if b == "1":
+            is_type1 = is_end_accent
+
+        if b == "1" and is_type1:
+            is_start_accent = True
+        elif b == "2" and not is_type1:
+            is_start_accent = True
+        else:
+            is_start_accent = False
+
+        phoneme_list[i].label = phoneme
+        start_accent_list.append(is_start_accent)
+        end_accent_list.append(is_end_accent)
 
     # yukarin_s
     with open("data/yukarin_s/config.yaml") as f:
@@ -52,7 +78,6 @@ def run(text: str, speaker_id: int):
     phoneme_length = generator_s.generate(
         phoneme_list=phoneme_list, speaker_id=speaker_id
     )
-    print(phoneme_length)
     numpy.save("hiho_phoneme_length.npy", phoneme_length)
 
     # yukarin_sos
@@ -61,23 +86,32 @@ def run(text: str, speaker_id: int):
 
     generator_sos = GeneratorSos(
         config=ConfigSos.from_dict(d),
-        predictor=Path("data/yukarin_sos/predictor_10000.pth"),
+        predictor=Path("data/yukarin_sos/predictor_70000.pth"),
         use_gpu=False,
     )
 
     rate = 200
 
     phoneme_list = numpy.r_[0, phoneme_list, 0]
+    start_accent_list = numpy.array(start_accent_list, dtype=numpy.int64)
+    end_accent_list = numpy.array(end_accent_list, dtype=numpy.int64)
     phoneme_length = numpy.r_[0.1, phoneme_length, 0.1]
     phoneme = numpy.repeat(
         phoneme_list, numpy.round(phoneme_length * rate).astype(numpy.int32)
     )
+    start_accent = numpy.repeat(
+        start_accent_list, numpy.round(phoneme_length * rate).astype(numpy.int32)
+    )
+    end_accent = numpy.repeat(
+        end_accent_list, numpy.round(phoneme_length * rate).astype(numpy.int32)
+    )
 
     f0 = generator_sos.generate(
         phoneme=phoneme[numpy.newaxis],
+        start_accent=start_accent[numpy.newaxis],
+        end_accent=end_accent[numpy.newaxis],
         speaker_id=numpy.array(speaker_id).reshape(1),
     )[0]
-    print(f0)
     numpy.save("hiho_f0.npy", f0)
 
     # yukarin_soso
@@ -86,7 +120,7 @@ def run(text: str, speaker_id: int):
 
     generator_soso = GeneratorSoso(
         config=ConfigSoso.from_dict(d),
-        predictor=Path("data/yukarin_soso/predictor_820000.pth"),
+        predictor=Path("data/yukarin_soso/predictor_120000.pth"),
         use_gpu=False,
     )
 
@@ -102,7 +136,6 @@ def run(text: str, speaker_id: int):
         phoneme=phoneme[numpy.newaxis],
         speaker_id=numpy.array(speaker_id).reshape(1),
     )[0]
-    print(spec)
     numpy.save("hiho_spec.npy", spec)
 
     # hifi-gan
@@ -114,6 +147,7 @@ def run(text: str, speaker_id: int):
 
     # save
     soundfile.write("hiho_output.wav", data=wave, samplerate=24000)
+    soundfile.write(f"{text}-{speaker_id}.wav", data=wave, samplerate=24000)
 
 
 if __name__ == "__main__":
